28b3595045142bd1920c1e44ad317521fff6ef12
Abhinav Sharma <abhi18av@gmail.com>
2020-10-25 18:59:16 +0000

    refine the step 04 and 05

---
+++ src/src/features/05_final_snp_df.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+
+# TODO use transposed df
+binary_unique_snps_df = pd.read_csv("../data/processed/binary_unique_snps_df.csv")
+
+binary_unique_snps_df = binary_unique_snps_df.rename({'Unnamed: 0': 'SampleID'}).set_index('SampleID')
+
+
+## Ensure that every column has atleast one mutation
+
+binary_col_mutation_dict = {}
+
+for col in binary_unique_snps_df.columns:
+    sum_of_col = binary_unique_snps_df[col].sum()
+    if  sum_of_col > 0:
+        # print('col: ', col, "\tsum: ", col_sum)
+        binary_col_mutation_dict[col] =  dict(binary_unique_snps_df[col].value_counts())
+        print(col)
+
+
+
+
+hetero_binary_vcf_snps_with_mutations_df= binary_unique_snps_df[list(binary_col_mutation_dict.keys())]
+
+hetero_binary_vcf_snps_with_mutations_df.to_csv("../data/processed/hetero_binary_vcf_snps_with_mutations_df.csv")
+
+
+## hetero_binary_vcf_snps_with_mutations_df = pd.read_csv("../data/processed/hetero_binary_vcf_snps_with_mutations_df.csv", index_col='SampleID')
+
+hetero_binary_vcf_snps_with_mutations_df = binary_unique_snps_df[list(binary_col_mutation_dict.keys())]
+
+
+#hetero_binary_vcf_snps_with_mutations_df = hetero_binary_vcf_snps_with_mutations_df.rename(columns= {'Unnamed: 0': 'SampleID'}).set_index('SampleID')
+
+
+## Creation of the final dataframe
+
+final_df = binarized_resistance_status_df.join(hetero_binary_vcf_snps_with_mutations_df)
+
+final_df.to_csv("../data/processed/binarized_final_df.csv")
+
