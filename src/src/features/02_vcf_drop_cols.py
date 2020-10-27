b5759f14ef5292e055ac2f17ded7d0eec0021fdd
Abhinav Sharma <abhi18av@gmail.com>
2020-10-27 14:33:24 +0530

    refactor for the final dataset

--- src/src/features/02_vcf_drop_cols.py
+++ src/src/features/02_vcf_drop_cols.py
@@ -6,14 +6,14 @@
 
 import pandas as pd
 
-vcf_df = pd.read_csv("../../data/raw/cohort.bqsr.filter.snps.tsv", sep='\t')
+vcf_df = pd.read_csv("../../data/raw/final.cohort.bqsr.filter.snps.tsv", sep='\t')
 
 vcf_df['CHROM.POS'] = vcf_df.apply(lambda row: row.CHROM + "." + str(row.POS), axis=1)
 vcf_df.drop(['CHROM', 'POS'], axis=1, inplace=True)
 
 vcf_df.set_index('CHROM.POS', inplace=True)
 vcf_df.columns = list(map(lambda column: column.split(".")[0], vcf_df.columns))
-vcf_df.to_csv("../../data/interim/cols_dropped_vcf_untransposed_df.tsv", "\t")
+vcf_df.to_csv("../../data/interim/final.cols_dropped_vcf_untransposed_df.tsv", "\t")
 
 vcf_df = vcf_df.transpose()
-vcf_df.to_csv("../../data/interim/cols_dropped_vcf_transposed_df.tsv", "\t")
+vcf_df.to_csv("../../data/interim/final.cols_dropped_vcf_transposed_df.tsv", "\t")
--- src/src/features/03_filter_unique_snps.py
+++ src/src/features/03_filter_unique_snps.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 
-vcf_snps_df = pd.read_csv("../../data/interim/cols_dropped_vcf_transposed_df.tsv", "\t")
+vcf_snps_df = pd.read_csv("../../data/interim/final.cols_dropped_vcf_transposed_df.tsv", "\t")
 
 vcf_snps_df = vcf_snps_df.rename(columns={'Unnamed: 0': 'SampleID'}).set_index('SampleID')
 
@@ -15,8 +15,8 @@ for col in vcf_snps_df.columns:
         col_mutation_dict[col] = dict(vcf_snps_df[col].value_counts())
         print(col)
 
-with open("../../data/interim/cols_with_mutations_dict.txt", "w") as outfile:
+with open("../../data/interim/final.cols_with_mutations_dict.txt", "w") as outfile:
     outfile.write(str(col_mutation_dict))
 
 vcf_unique_snps_df = vcf_snps_df[list(col_mutation_dict.keys())]
-vcf_unique_snps_df.to_csv("../../data/interim/vcf_unique_snps_df.tsv", "\t")
+vcf_unique_snps_df.to_csv("../../data/interim/final.vcf_unique_snps_df.tsv", "\t")
